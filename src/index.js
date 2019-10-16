7c037ff3a4103e8a07760cc5873714073d12f5f3
Tom Rees-Herdman <tom@techanvil.co.uk>
2019-10-16 01:44:08 +0100

    Forward event to Mixpanel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { Validator, ValidationError } = require('express-json-validator-middleware');
+const {Validator, ValidationError} = require('express-json-validator-middleware');
+
+const {mixpanelTrackEvent} = require('./mixpanel');
 
 const validator = new Validator({allErrors: true});
 
@@ -16,13 +18,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(eventMiddleware);
 
-app.post('/event/', validator.validate({geckoEvent: ({geckoSchema}) => geckoSchema}), function(req, res) {
-  res.send('valid');
+app.post('/event/', validator.validate({geckoEvent: ({geckoSchema}) => geckoSchema}), async function(req, res) {
+  const isValid = await mixpanelTrackEvent('FOO', req.geckoEvent);
+  res.send((isValid ? 'valid' : 'invalid') + ' event');
 });
 
 app.use(function(err, req, res, next) {
   if (err instanceof ValidationError) {
-    res.status(400).send('invalid');
+    res.status(400).send('JSON schema validation error');
     next();
   }
   else next(err);
